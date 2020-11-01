76e99d3be9f71a842e453abd07e2dec9eadd5b48
Leandro (Cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2020-11-01 03:02:51 -0300

    source code now shows as preformatted text

--- src/rosetta-lang/index.ts
+++ src/rosetta-lang/index.ts
@@ -1,18 +1,18 @@
 import { default as Parser } from "./parser";
 
+var curr_code = `\
+public:Bool is_valid_color(color_code, max_range, min_range? 0: Number)
+    ret min_range < color_code < max_range`;
 
 class Main {
     constructor() {
-        let curr_code = `public:Bool is_valid_color(color_code, max_range, min_range? 0: Number)
-        ret min_range < color_code < max_range`;
-
-        let out: Parser = new Parser(curr_code);
-
-        let code_element: HTMLDivElement = document.createElement("div");
-        code_element.id = "";
+        let code_element: HTMLPreElement = document.createElement("pre");
+        code_element.id = "source-code";
         code_element.innerHTML = curr_code;
 
         document.body.appendChild(code_element);
+
+        new Parser(curr_code);
     }
 }
 
